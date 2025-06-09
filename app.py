e2669621ad183b6432893e14e68280791c4efe0d
gomugomucode <alinedtwins@gmail.com>
2025-06-09 16:38:07 +0545

    adding login and register.html files

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def index():
     return render_template('index.html', tasks=tasks, query=query)
 
 @app.route('/add', methods=['GET', 'POST'])
+@login_required
 def add_task():
     if request.method == 'POST':
         title = request.form['title']
@@ -29,11 +30,13 @@ def add_task():
     return render_template('add_task.html')
 
 @app.route('/delete/<int:index>')
+@login_required
 def delete_task(index):
     tm.delete_task(index)
     return redirect(url_for('index'))
 
 @app.route('/toggle/<int:index>')
+@login_required
 def toggle_task(index):
     tm.toggle_task(index)  
     return redirect(url_for('index'))
